1b91957ba75444737b3b628e6c8aeec3afb1132d
Soumalya Paul <54816468+soumalya340@users.noreply.github.com>
2022-12-21 22:31:18 +0530

    Added Royalty

--- WhitelistNft.sol
+++ WhitelistNft.sol
@@ -4,21 +4,24 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+
+import "./@rarible/contracts/RoyaltiesV2.sol";
+import "./@rarible/contracts/IERC2981.sol";
+import "./@rarible/contracts/LibRoyaltiesV2.sol";
+import "./@rarible/contracts/LibRoyaltiesV2.sol";
+import "./@rarible/contracts/LibPart.sol";
 import "./IERC4907.sol";
 
-contract NFT is ERC721, Ownable ,IERC4907  {
-    
-    struct UserInfo 
-    {
-        address user;   // address of user role
+contract NFT is ERC721, Ownable, IERC4907 {
+    struct UserInfo {
+        address user; // address of user role
         uint64 expires; // unix timestamp, user expires
     }
 
-    mapping (uint256  => UserInfo) internal _users;
+    mapping(uint256 => UserInfo) internal _users;
 
     using Counters for Counters.Counter;
     uint256 constant maxSupply = 2000;
-
 
     string private revealURI;
     uint256 private totalSupply;
@@ -35,6 +38,8 @@ contract NFT is ERC721, Ownable ,IERC4907  {
 
     bool public revealed = false;
 
+    bytes private constant _INTERFACE_ID_ERC2981 = 0x2a55205a;
+
     Counters.Counter private _tokenIdCounter;
 
     constructor(
@@ -46,36 +51,34 @@ contract NFT is ERC721, Ownable ,IERC4907  {
         publicprice = _publicprice;
         allowListprice = _allowListprice;
     }
+
     modifier whenNotpause() {
         require(pause == true, "The minting is stopped");
         _;
     }
 
-
-    function setRoyalty(uint _royaltypercentage) public onlyOwner{
+    function setRoyalty(uint _royaltypercentage) public onlyOwner {
         royaltypercentage = _royaltypercentage;
     }
 
-
-    function setRevealUri(string memory _uri) external onlyOwner{
+    function setRevealUri(string memory _uri) external onlyOwner {
         revealURI = _uri;
     }
 
-
-    function reveal()  external onlyOwner {
+    function reveal() external onlyOwner {
         revealed = true;
     }
 
-    //reveal the token URI by overriding the function 
-    function tokenURI(uint256 tokenId) public view virtual override  returns (string memory) {
-        require(tokenId <= totalSupply,"non existent toekn");
-        if(revealed == true){
+    //reveal the token URI by overriding the function
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override returns (string memory) {
+        require(tokenId <= totalSupply, "non existent toekn");
+        if (revealed == true) {
             return super.tokenURI(tokenId);
-        }
-        else{
+        } else {
             return revealURI;
         }
-
     }
 
     function Pause(bool _pause) external onlyOwner {
@@ -139,19 +142,24 @@ contract NFT is ERC721, Ownable ,IERC4907  {
         }
     }
 
-
     /* RENTABLE */
-    
+
     /// @notice set the user and expires of an NFT
     /// @dev The zero address indicates there is no user
     /// Throws if `tokenId` is not valid NFT
     /// @param user  The new user of the NFT
     /// @param expires  UNIX timestamp, The new user could use the NFT before expires
 
-    
-    function setUser(uint256 tokenId, address user, uint64 expires) public override virtual{
-        require(_isApprovedOrOwner(msg.sender, tokenId), "ERC4907: transfer caller is not owner nor approved");
-        UserInfo storage info =  _users[tokenId];
+    function setUser(
+        uint256 tokenId,
+        address user,
+        uint64 expires
+    ) public virtual override {
+        require(
+            _isApprovedOrOwner(msg.sender, tokenId),
+            "ERC4907: transfer caller is not owner nor approved"
+        );
+        UserInfo storage info = _users[tokenId];
         info.user = user;
         info.expires = expires;
         emit UpdateUser(tokenId, user, expires);
@@ -161,28 +169,33 @@ contract NFT is ERC721, Ownable ,IERC4907  {
     /// @dev The zero address indicates that there is no user or the user is expired
     /// @param tokenId The NFT to get the user address for
     /// @return The user address for this NFT
-    function userOf(uint256 tokenId) public view override virtual returns(address){
-        if( uint256(_users[tokenId].expires) >=  block.timestamp){
-            return  _users[tokenId].user;
-        }
-        else{
+    function userOf(
+        uint256 tokenId
+    ) public view virtual override returns (address) {
+        if (uint256(_users[tokenId].expires) >= block.timestamp) {
+            return _users[tokenId].user;
+        } else {
             return address(0);
         }
     }
-
-    
 
     /// @notice Get the user expires of an NFT
     /// @dev The zero value indicates that there is no user
     /// @param tokenId The NFT to get the user expires for
     /// @return The user expires for this NFT
-    function userExpires(uint256 tokenId) public view override virtual returns(uint256){
+    function userExpires(
+        uint256 tokenId
+    ) public view virtual override returns (uint256) {
         return _users[tokenId].expires;
     }
 
     /// @dev See {IERC165-supportsInterface}.
-    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
-        return interfaceId == type(IERC4907).interfaceId || super.supportsInterface(interfaceId);
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override returns (bool) {
+        return
+            interfaceId == type(IERC4907).interfaceId ||
+            super.supportsInterface(interfaceId);
     }
 
     //@notice If the owner wants to transfer the there should be zero users for that token
@@ -192,13 +205,37 @@ contract NFT is ERC721, Ownable ,IERC4907  {
         uint256 tokenId,
         bytes memory data
     ) public virtual override {
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner or approved");
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: caller is not token owner or approved"
+        );
         super._safeTransfer(from, to, tokenId, data);
         if (from != to && _users[tokenId].expires >= block.timestamp) {
             delete _users[tokenId];
             emit UpdateUser(tokenId, address(0), 0);
         }
     }
+
+    /**** ROYALTIES******/
+    function setRoyalties(
+        uint _tokenId,
+        address payable _royaltiesReceipientAddress,
+        uint96 _percentageBasisPoints
+    ) public onlyOwner {
+        LibPart.Part memory _royalties = new LibPart.Part[](1);
+        _royalties[0].value = _percentageBasisPoints;
+        _royalties[1].value = _royaltiesReceipientAddress;
+        _saveRoyalties(_tokenId, _royalties);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721) returns (bool) {
+    if(interface == LibRoyalties2981_INTERFACE_ID_ROYALTIES)
+        return true;
+    if(interface == _INTERFACE_ID_ERC2981)
+       return true;
+    return super.supportsInterface(interfaceId);
+    }
+}
 
 
     // The following functions are overrides required by Solidity.
@@ -207,7 +244,10 @@ contract NFT is ERC721, Ownable ,IERC4907  {
         return baseURI;
     }
 
-    function calculateRoyaltyFee(uint _publicprice , uint _royaltypercentage) internal pure returns (uint256) {
-        return (_publicprice *  _royaltypercentage) / 100;
+    function calculateRoyaltyFee(
+        uint _publicprice,
+        uint _royaltypercentage
+    ) internal pure returns (uint256) {
+        return (_publicprice * _royaltypercentage) / 100;
     }
 }
